b67712d8ed9119893440de7c235f8dff79e76bb3
Mikhail Iakovlev <mikhail.iakovlev@student.manchester.ac.uk>
2017-10-06 14:54:17 +0100

    Migration to Expo

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import Store from './Store';
-import BaseNavigation from './Router';
+import Store from './src/Store';
+import BaseNavigation from './src/Router';
 
 const App = () => (
   <Provider store={Store}>
